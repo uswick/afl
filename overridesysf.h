835771fb40e7f011bab7d5427022743dfe697fcd
uwickramasin <uwickramasin@uwickramasin-dev>
2019-06-21 15:33:13 -0700

    add header for macro override identifiers

---
+++ overridesysf.h
@@ -0,0 +1,14 @@
+#ifndef __H_OVERRIDE_SYS_FUNCS_H__
+#define __H_OVERRIDE_SYS_FUNCS_H__
+
+#define gettimeofday 	gettimeofday
+#define getenv 		getenv
+#define fopen         	fopen
+#define fgets    	fgets
+#define symlink 	symlink
+#define unlink 		unlink
+#define lseek           lseek 
+#define link 		link
+#define read 		read
+
+#endif
