b08115672c0a3d2fe5395d1121a5521bcc2a6056
uwickramasin <uwickramasin@uwickramasin-dev>
2019-06-21 16:00:05 -0700

    add more identifiers for override

--- overridesysf.h
+++ overridesysf.h
@@ -4,11 +4,18 @@
 #define gettimeofday 	gettimeofday
 #define getenv 		getenv
 #define fopen         	fopen
+#define fdopen         	fdopen
+#define open         	open
+#define opendir         opendir
+#define readdir         readdir
 #define fgets    	fgets
+#define lseek           lseek 
+#define fclose 		fclose
 #define symlink 	symlink
 #define unlink 		unlink
-#define lseek           lseek 
 #define link 		link
 #define read 		read
+#define mmap 		mmap
+#define munmap 		munmap
 
 #endif
