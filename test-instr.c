be473453d6e07821d5b9a49fe8d27a600703d2b5
uwickramasin <uwickramasin@uwickramasin-dev>
2019-06-29 18:58:01 -0700

    changes

--- test-instr.c
+++ test-instr.c
@@ -15,23 +15,40 @@
  */
 
 #include <stdio.h>
+#include <stdint.h>
 #include <stdlib.h>
 #include <unistd.h>
+
+#define MAP_SZ (1UL << 16)
+
+char buff[MAP_SZ];
+
+uint64_t AFLTraceUserVPN ;
+
+static inline void PrintStats(){
+  printf("SHM buffer=%p size=%lu map[0xbeef]=%u\n", buff, 
+		  MAP_SZ, buff[0xbeef]);
+}
 
 int main(int argc, char** argv) {
 
   char buf[8];
-
+  PrintStats();
   if (read(0, buf, 8) < 1) {
     printf("Hum?\n");
     exit(1);
   }
 
-  if (buf[0] == '0')
+  if (buf[0] == '0') {
+    PrintStats();
     printf("Looks like a zero to me!\n");
-  else
+  }
+  else{
     printf("A non-zero value? How quaint!\n");
+    PrintStats();
+  }
 
+  PrintStats();
   exit(0);
 
 }
